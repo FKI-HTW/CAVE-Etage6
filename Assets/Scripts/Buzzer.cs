3edd8819261c2879f5f9cdde48afb27372f2b59c
Julian.Knepel <julian.knepel@htw-berlin.de>
2024-06-18 16:12:08 +0200

    feat: added sound to cave wall animation

--- Assets/Scripts/Buzzer.cs
+++ Assets/Scripts/Buzzer.cs
@@ -5,11 +5,13 @@ namespace HTW.CAVE.Etage6App
     public class Buzzer : MonoBehaviour
     {
         [SerializeField] private Animation wallAnimation;
+        [SerializeField] private AudioSource _audioSource;
         private bool _wallsRaised = true;
 
         private void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("HandCollider")) return;
+            _audioSource.Play();
             wallAnimation.Play(_wallsRaised ? "lower_walls" : "raise_walls");
             _wallsRaised = !_wallsRaised;
         }
